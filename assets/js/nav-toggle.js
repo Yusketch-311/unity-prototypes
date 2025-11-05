361ae21b46f116c9bd969a503e3c128a6d32265e
Yusketch-311 <t.yusuke31@gmail.com>
2025-11-05 15:22:20 +0900

    replace all

--- assets/js/nav-toggle.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const toggleButton = document.getElementById('nav-toggle');
-  const nav = document.getElementById('site-nav');
-
-  toggleButton.addEventListener('click', function () {
-    nav.classList.toggle('active');
-  });
-});
